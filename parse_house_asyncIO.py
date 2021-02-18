770e68f94eea530fe1a4bc08389223f3b01d672e
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-02-18 13:23:18 -0300

    added assync html parser to fetch all houses informations

--- parse_house_asyncIO.py
+++ parse_house_asyncIO.py
@@ -36,7 +36,6 @@ async def fetch_all(urls, loop):
             *[fetch(session, url) for url in urls], return_exceptions=True
         )
         return results
-    # return BeautifulSoup(results.decode("utf-8"), "html5lib")
 
 
 if __name__ == "__main__":
@@ -44,23 +43,39 @@ if __name__ == "__main__":
     urls = houses_urls
     htmls = loop.run_until_complete(fetch_all(urls, loop))
     for html in htmls:
+        titulo = utils.extract_titulo(html)
         dormitorios = utils.extract_dormitorios(html)
+        comodos = utils.extract_information(html, span_pattern="bed")
+        banheiros = utils.extract_information(html, span_pattern="shower")
+        area_m2 = utils.extract_information(html, span_pattern="arrows")
+        vagas_garagem = utils.extract_information(html, span_pattern="car")
+        valor_imovel = utils.extract_valor_imovel(html)
+        iptu = utils.extract_iptu(html)
+
+        # update vectors
+        TITULO.append(titulo)
         DORMITORIOS.append(dormitorios)
+        COMODOS.append(comodos)
+        BANHEIROS.append(banheiros)
+        AREA_M2.append(area_m2)
+        VAGAS_GARAGEM.append(vagas_garagem)
+        VALOR_IMOVEL.append(valor_imovel)
+        IPTU.append(iptu)
 
     # Build Dataframe
     data_dict = {
         "url": HOUSES_URLS,
-        # "titulo": TITULO,
+        "titulo": TITULO,
         "dormitorios": DORMITORIOS,
-        # "comodos": COMODOS,
-        # "banheiros": BANHEIROS,
-        # "area_m2": AREA_M2,
-        # "vagas_garagem": VAGAS_GARAGEM,
-        # "valor_imovel": VALOR_IMOVEL,
-        # "iptu": IPTU,
+        "comodos": COMODOS,
+        "banheiros": BANHEIROS,
+        "area_m2": AREA_M2,
+        "vagas_garagem": VAGAS_GARAGEM,
+        "valor_imovel": VALOR_IMOVEL,
+        "iptu": IPTU,
     }
 
+    # Export data to .csv
     data = pd.DataFrame.from_dict(data_dict)
     now = datetime.now().strftime("%Y%m%d")
-    print(data.head())
     data.to_csv(f"dataframe_houses_brognoly_{now}.csv", index=False)
--- utils.py
+++ utils.py
@@ -11,3 +11,43 @@ def extract_dormitorios(text):
         return dormitorios
     except:
         return None
+
+
+def extract_titulo(text):
+    soup = BeautifulSoup(text.decode("utf-8"), "lxml")
+    titulo = soup.find("div", {"class": "titulo-imovel"})
+    titulo = titulo.text.strip()
+    return titulo
+
+
+def extract_information(text, span_pattern):
+    soup = BeautifulSoup(text.decode("utf-8"), "lxml")
+    info = soup.find("ul", {"class": "features gamd-features"}).find_all("li")
+    information = None
+    for i in info:
+        if span_pattern in str(i.span):
+            information = i.div.text.replace("\xa0", "")
+            information = re.search(r"\d+", information).group()
+    return information
+
+
+def extract_valor_imovel(text):
+    soup = BeautifulSoup(text.decode("utf-8"), "lxml")
+    valores = soup.find("div", {"class": "valoresImovel cardEstilo"}).find_all("p")
+    valor_imovel = valores[0].text
+    try:
+        valor_imovel = float(re.search("R\$(\d*.?\d*)", valor_imovel).group(1))
+    except:
+        valor_imovel = None
+    return valor_imovel
+
+
+def extract_iptu(text):
+    soup = BeautifulSoup(text.decode("utf-8"), "lxml")
+    valores = soup.find("div", {"class": "valoresImovel cardEstilo"}).find_all("p")
+    iptu = valores[1].text
+    try:
+        iptu = float(re.search("R\$(\d*.?\d*)", iptu).group(1))
+    except:
+        iptu = None
+    return iptu
